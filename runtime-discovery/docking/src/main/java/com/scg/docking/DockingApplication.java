2da45b5f3efc361e3a539fa90dbdbaea1ef4e7b6
Rick Hendriks <Rick Hendriks@host.docker.internal>
2020-06-01 00:49:48 +0200

    docking

--- runtime-discovery/docking/src/main/java/com/scg/docking/DockingApplication.java
+++ runtime-discovery/docking/src/main/java/com/scg/docking/DockingApplication.java
@@ -2,10 +2,8 @@ package com.scg.docking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class DockingApplication {
 
 	public static void main(String[] args) {
