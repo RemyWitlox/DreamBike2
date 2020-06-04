8ddf29f3b6bf7ccc43905b174f3424973a6d1aae
Rick Hendriks <Rick Hendriks@host.docker.internal>
2020-06-04 18:04:12 +0200

    docking

--- runtime-discovery/docking/src/main/java/com/scg/docking/DockingController.java
+++ runtime-discovery/docking/src/main/java/com/scg/docking/DockingController.java
@@ -3,7 +3,6 @@ package com.scg.docking;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin("*")
+//@CrossOrigin("*")
 public class DockingController {
 
 	private final DockingService dockingService;
