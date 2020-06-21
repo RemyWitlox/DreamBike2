ef77d21bab6b542c3594dd71df6672b060de839a
Rick Hendriks <Rick Hendriks@host.docker.internal>
2020-06-21 20:32:58 +0200

    loginauth

--- runtime-discovery/gateway/src/main/java/com/scg/gateway/Gateway.java
+++ runtime-discovery/gateway/src/main/java/com/scg/gateway/Gateway.java
@@ -1,8 +1,14 @@
 package com.scg.gateway;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,5 +16,16 @@ public class Gateway {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Gateway.class, args);
+	}
+
+	@RestController
+	public class restController {
+		@GetMapping("/connect")
+		public boolean connect() throws IOException {
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+			LocalDateTime now = LocalDateTime.now();
+			System.out.println(dtf.format(now) + " Connection tested");
+			return true;
+		}
 	}
 }
