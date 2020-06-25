8fca1814aee790878c40be00792815c1c0e1338e
Rick Hendriks <Rick Hendriks@host.docker.internal>
2020-06-25 22:55:54 +0200

    test

--- runtime-discovery/gateway/src/main/java/com/scg/gateway/Gateway.java
+++ runtime-discovery/gateway/src/main/java/com/scg/gateway/Gateway.java
@@ -1,14 +1,8 @@
 package com.scg.gateway;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -18,14 +12,14 @@ public class Gateway {
 		SpringApplication.run(Gateway.class, args);
 	}
 
-	@RestController
-	public class restController {
-		@GetMapping("/connect")
-		public boolean connect() throws IOException {
-			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-			LocalDateTime now = LocalDateTime.now();
-			System.out.println(dtf.format(now) + " Connection tested");
-			return true;
-		}
-	}
+//	@RestController
+//	public class restController {
+//		@GetMapping("/connect")
+//		public boolean connect() throws IOException {
+//			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+//			LocalDateTime now = LocalDateTime.now();
+//			System.out.println(dtf.format(now) + " Connection tested");
+//			return true;
+//		}
+//	}
 }
