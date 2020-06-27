d73cd3a357a648306f6ae0fd3ca3991e2a43ad21
Rick Hendriks <Rick Hendriks@host.docker.internal>
2020-06-27 22:04:13 +0200

    testen dan maar weer

--- runtime-discovery/gateway/src/main/java/com/scg/gateway/Gateway.java
+++ runtime-discovery/gateway/src/main/java/com/scg/gateway/Gateway.java
@@ -3,6 +3,11 @@ package com.scg.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter.Mode;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,14 +17,24 @@ public class Gateway {
 		SpringApplication.run(Gateway.class, args);
 	}
 
-//	@RestController
-//	public class restController {
-//		@GetMapping("/connect")
-//		public boolean connect() throws IOException {
-//			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-//			LocalDateTime now = LocalDateTime.now();
-//			System.out.println(dtf.format(now) + " Connection tested");
-//			return true;
-//		}
-//	}
+	@Bean
+	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
+			ReactiveClientRegistrationRepository clientRegistrationRepository) {
+		// Authenticate through configured OpenID Provider
+		http.oauth2Login();
+
+		// Also logout at the OpenID Connect provider
+//		http.logout(logout -> logout
+//				.logoutSuccessHandler(new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository)));
+
+		// Require authentication for all requests
+		http.authorizeExchange().anyExchange().authenticated();
+
+		// Allow showing /home within a frame
+		http.headers().frameOptions().mode(Mode.SAMEORIGIN);
+
+		// Disable CSRF in the gateway to prevent conflicts with proxied service CSRF
+		http.csrf().disable();
+		return http.build();
+	}
 }
