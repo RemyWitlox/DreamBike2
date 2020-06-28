f8266c9c58c0794dc6673ca26827f96750ca351a
Rick Hendriks <Rick Hendriks@host.docker.internal>
2020-06-28 11:46:48 +0200

    test

--- runtime-discovery/docking/src/main/java/com/scg/docking/config/SecurityConfigurer.java
+++ runtime-discovery/docking/src/main/java/com/scg/docking/config/SecurityConfigurer.java
@@ -1,5 +1,11 @@
 package com.scg.docking.config;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
@@ -9,11 +15,13 @@ import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
@@ -41,8 +49,18 @@ public class SecurityConfigurer extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(final HttpSecurity http) throws Exception {
 
+//		http.cors().configurationSource(corsConfigurationSource()).and().headers().frameOptions().disable().and().csrf()
+//				.disable().authorizeRequests().antMatchers(securityProperties.getApiMatcher()).authenticated()
+//				.antMatchers("*").permitAll();
 		http.cors().configurationSource(corsConfigurationSource()).and().headers().frameOptions().disable().and().csrf()
-				.disable().authorizeRequests().antMatchers(securityProperties.getApiMatcher()).authenticated();
+				.disable().authorizeRequests().antMatchers("*").permitAll().and().formLogin()
+				.successHandler(new AuthenticationSuccessHandler() {
+					@Override
+					public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+							Authentication authentication) throws IOException, ServletException {
+						// do nothing
+					}
+				});
 	}
 
 	@Bean
