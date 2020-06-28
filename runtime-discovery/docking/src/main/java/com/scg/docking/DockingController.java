95bb0e589df9eb22861c7a9c841a41c097b0744a
unknown <jmgwitlox@hotmail.com>
2020-06-28 13:06:25 +0200

    test

--- runtime-discovery/docking/src/main/java/com/scg/docking/DockingController.java
+++ runtime-discovery/docking/src/main/java/com/scg/docking/DockingController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +34,7 @@ public class DockingController {
 		return "test";
 	}
 
+	@PreAuthorize("hasAnyRole('USER', 'ADMIN','EMPLOYEE,'MANAGEMENT')")
 	@GetMapping("getAll")
 	public List<DockingStation> getAllDockingStations() {
 //		return this.dockingService.getAll();
